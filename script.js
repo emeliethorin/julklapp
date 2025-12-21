a66a77ae07f789b244575703d206ca445ee127c9
emeliethorin <emeliethorin10@gmail.com>
2025-12-21 20:22:06 +0100

    update js

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const unlockBtn = document.getElementById('unlockBtn');
 const gift = document.getElementById('gift');
 const present = document.getElementById('present');
 
+let unlocked = false;
+
 unlockBtn.addEventListener('click', () => {
-  gift.textContent = '🎉🎁';
-  gift.classList.add('open');
-  present.classList.add('show');
+  if (!unlocked) {
+    gift.textContent = '🎉';
+    gift.classList.add('open');
+
+    present.classList.add('show'); // 👈 EXPAND HÄR
+
+    unlockBtn.textContent = 'LADDA OM 🔁';
+    unlocked = true;
+  } else {
+    location.reload();
+  }
 });
